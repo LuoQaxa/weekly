e082b470c55b86de3a9f561a477302165503e3a9
luoqian18 <luoqian18@meituan.com>
2024-02-06 15:21:10 +0800

    feat: 处理冲突

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,11 @@
-import rss from '@astrojs/rss';
+import rss from "@astrojs/rss";
 
-let allPosts = import.meta.glob('./posts/*.md', { eager: true });
+let allPosts = import.meta.glob("./posts/*.md", { eager: true });
 let posts = Object.values(allPosts);
 posts = posts.sort((a, b) => {
   return (
-    parseInt(b.url.split('/posts/')[1].split('-')[0]) -
-    parseInt(a.url.split('/posts/')[1].split('-')[0])
+    parseInt(b.url.split("/posts/")[1].split("-")[0]) -
+    parseInt(a.url.split("/posts/")[1].split("-")[0])
   );
 });
 
@@ -14,15 +14,15 @@ posts.splice(10);
 
 export const get = () =>
   rss({
-    title: '潮流周刊',
-    description: '记录 Tw93 潮流前端的日常生活',
-    site: 'https://weekly.tw93.fun/',
+    title: "生活周刊",
+    description: "记录 LuoQ 工程师的日常生活",
+    site: "https://thatdog.cn/",
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
-      const oldTitle = url.split('/posts/')[1];
+      const oldTitle = url.split("/posts/")[1];
       const title =
-        '第' + oldTitle.split('-')[0] + '期 - ' + oldTitle.split('-')[1];
+        "第" + oldTitle.split("-")[0] + "期 - " + oldTitle.split("-")[1];
       return {
         link: url,
         title,
