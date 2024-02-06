acf9fcf88e147b70ccc4cb2c52aa0398762395b9
luoqian18 <luoqian18@meituan.com>
2024-02-06 14:29:57 +0800

    feat: 替换原作者信息

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ import datetime
 
 def fetch_ci_time(filePath):
     entries = httpx.get(
-        "https://api.github.com/repos/tw93/weekly/commits?path=" + filePath + "&page=1&per_page=1")
+        "https://api.github.com/repos/LuoQaxa/weekly/commits?path=" + filePath + "&page=1&per_page=1")
     ciTime = entries.json()[0]["commit"]["committer"]["date"].split("T")[0]
     return ciTime
     # return datetime.datetime.strptime(ciTime,"%Y-%m-%d")
@@ -16,7 +16,7 @@ def fetch_ci_time(filePath):
 if __name__ == "__main__":
     readmefile = open('README.md', 'w')
     readmefile.write(
-        "# 潮流周刊\n\n> 记录工程师 Tw93 的不枯燥生活，欢迎订阅，也欢迎 [推荐](https://github.com/tw93/weekly/discussions/22) 你的好东西，Fork 自用可见 [开发文档](https://github.com/tw93/weekly/blob/main/Deploy.md)，期待你玩得开心~\n\n")
+        "# 生活周刊\n\n> 记录工程师 LuoQ 的不枯燥生活，欢迎订阅～\n\n")
     recentfile = open('RECENT.md', 'w')
 
     for root, dirs, filenames in os.walk('./src/pages/posts'):
@@ -27,18 +27,14 @@ if __name__ == "__main__":
         if name.endswith('.md'):
             filepath = urllib.parse.quote(name)
             oldTitle = name.split('.md')[0]
-            url = 'https://weekly.tw93.fun/posts/' + oldTitle
+            url = 'https://thatdog.cn/posts/' + oldTitle
             title = '第 ' + \
                 oldTitle.split('-')[0] + ' 期 - ' + oldTitle.split('-')[1]
             readmeMd = '* [{}]({})\n'.format(title, url)
-            dateList = ["2022-10-10", "2022-09-26",
-                        "2022-09-12", "2022-09-05", "2022-08-29"]
+  
             num = int(oldTitle.split('-')[0])
             if index < 5:
-                if num < 100:
-                    modified = dateList[99-num]
-                else:
-                    modified = fetch_ci_time('/src/pages/posts/' + filepath)
+                modified = fetch_ci_time('/src/pages/posts/' + filepath)
 
                 recentMd = '* [{}]({}) - {}\n'.format(title, url, modified)
                 recentfile.write(recentMd)
