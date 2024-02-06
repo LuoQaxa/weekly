a2134e6ded538315bf77e912db9fac7d9a931945
luoqian18 <luoqian18@meituan.com>
2024-02-06 15:31:33 +0800

    fix: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,15 @@ import { defineConfig } from "astro/config";
 import { parse } from "node-html-parser";
 import { SITE } from "./src/config";
 
+const DEFAULT_FORMAT = "YYYY/MM/DD";
+function getCreateDateFormat(filePath) {
+  return dayjs(fs.statSync(filePath).birthtime).format(DEFAULT_FORMAT);
+}
+
 function defaultLayoutPlugin() {
   return function (tree, file) {
     const filePath = file.history[0];
+    const { frontmatter } = file.data.astro;
     file.data.astro.frontmatter.layout = "@layouts/post.astro";
 
     // 头图放到文档中的第一行，会自动帮你处理，也可以用 frontmatter 方式，赋值给 pic 字段
@@ -25,24 +31,21 @@ function defaultLayoutPlugin() {
     frontmatter.pic = frontmatter.pic || SITE.pic;
 
     if (!frontmatter.date) {
-      frontmatter.date =
-        SITE.repo === WEEKLY_REPO_NAME
-          ? getWeeklyDateFormat(filePath.split("/posts/")[1].split("-")[0])
-          : getCreateDateFormat(filePath);
+      frontmatter.date = SITE.repo === getCreateDateFormat(filePath);
     }
 
     // 兼容没有头图的情况
-    if (!pic) {
-      file.data.astro.frontmatter.pic = SITE.pic;
-    }
+    // if (!pic) {
+    //   file.data.astro.frontmatter.pic = SITE.pic;
+    // }
 
-    //这里也可以直接在 frontmatter，赋值给 date 字段
-    if (!date) {
-      const createDate = dayjs(fs.statSync(filePath).birthtime).format(
-        "YYYY/MM/DD"
-      );
-      file.data.astro.frontmatter.date = createDate;
-    }
+    // //这里也可以直接在 frontmatter，赋值给 date 字段
+    // if (!date) {
+    //   const createDate = dayjs(fs.statSync(filePath).birthtime).format(
+    //     "YYYY/MM/DD"
+    //   );
+    //   file.data.astro.frontmatter.date = createDate;
+    // }
   };
 }
 
